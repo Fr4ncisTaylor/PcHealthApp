e56d669861017c494870087b5cc371a6116741dc
Francis Taylor <francistrapp2000@gmail.com>
2026-02-27 16:28:33 -0300

    up

---
+++ config.py
@@ -0,0 +1,18 @@
+version = '0.0.1 BETA'
+default_lang = "pt"
+window_size = (700, 600)
+COLOR_MAP = {
+		"Blue": "#4fc3f7",
+		"Purple": "#b388ff",
+		"Green": "#69f0ae",
+		"Orange": "#ffb74d",
+		"Red": "#ff5252"
+	}
+
+ddr_map = {
+		20: "DDR",
+		21: "DDR2",
+		24: "DDR3",
+		26: "DDR4",
+		34: "DDR5"
+	}
